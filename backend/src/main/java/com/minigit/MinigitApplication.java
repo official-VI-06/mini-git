e9c910b1873d9d8060bda28f5dcc14e2a084e017
Yashvi Dalal <yashviextrawork@gmail.com>
2026-03-23 12:09:06 +0530

    initial project setup: spring boot backend + react frontend

---
+++ backend/src/main/java/com/minigit/MinigitApplication.java
@@ -0,0 +1,13 @@
+package com.minigit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinigitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinigitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/minigit/MinigitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minigit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinigitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
